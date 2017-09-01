4d4b12b31970634c99777888b53ef6abf96abd31
Doni Ramadhan <doniramadhan@gmail.com>
2017-09-01 11:59:12 +0200

    Initial Commit

---
+++ MiniTokopedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniTokopedia
+//
+//  Created by Doni on 2017-09-01.
+//  Copyright © 2017 Doni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
