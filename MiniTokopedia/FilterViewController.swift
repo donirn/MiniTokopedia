10f65383941fc98b8f98b12bbf0609312cad75fe
Doni Ramadhan <doniramadhan@gmail.com>
2017-09-02 17:08:41 +0200

    update price slider label

--- MiniTokopedia/FilterViewController.swift
+++ MiniTokopedia/FilterViewController.swift
@@ -7,10 +7,26 @@
 //
 
 import UIKit
+import RangeSeekSlider
 
 class FilterViewController: UIViewController {
+    @IBOutlet weak var minPriceLabel: UILabel!
+    @IBOutlet weak var maxPriceLabel: UILabel!
+    @IBOutlet weak var priceSlider: RangeSeekSlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        priceSlider.delegate = self
+    }
     
     @IBAction func closeButtonDidTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension FilterViewController: RangeSeekSliderDelegate{
+    func rangeSeekSlider(_ slider: RangeSeekSlider, didChange minValue: CGFloat, maxValue: CGFloat) {
+        minPriceLabel.text = "Rp \(Int(minValue))"
+        maxPriceLabel.text = "Rp \(Int(maxValue))"
     }
 }
