c44030b9297d846dc31a525548967dd871b12672
Doni Ramadhan <doniramadhan@gmail.com>
2017-09-02 13:32:15 +0200

    added Filter page that shows as a modal view

---
+++ MiniTokopedia/FilterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FilterViewController.swift
+//  MiniTokopedia
+//
+//  Created by Doni on 2017-09-02.
+//  Copyright © 2017 Doni. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    
+    @IBAction func closeButtonDidTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
